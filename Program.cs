e2653ae1ac5d8702acdf5ca8c846d26031a693f2
Fifi Kumalasari <fifi.kumalasari@agilis.id>
2023-11-29 12:56:03 +0700

    init project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Polindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input data : ");
+                string input = Console.ReadLine();
+                //string input = "rumah";
+
+                if (IsPalindrome(input.ToLower()))
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }   
+        }
+        public static bool IsPalindrome(string str)
+        {
+            return str.SequenceEqual(str.Reverse());
+        }
+    }
+}
